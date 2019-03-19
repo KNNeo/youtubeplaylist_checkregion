3384cde8d5237d580a43eb1c5564f4399838b30a
Neo Kai Neng <neo.kai.neng@gmail.com>
2019-03-19 17:51:26 +0800

    first version fixes
    ~fixed clearer results for videos not fully region free
    ~fixed video title encoding
    ~future improvements include obtain full region list and compile region blocked inline

--- batch_youtube_playlist_checkregion-final.py
+++ batch_youtube_playlist_checkregion-final.py
@@ -1,20 +1,19 @@
 import urllib.request, json, sys, datetime, time
 import numpy as np
 
-API_KEY = "INSERT API KEY HERE (YOUTUBE APIv3)"
+API_KEY = "Obtain YouTube API v3 KEY from cloud.google.com"
 non_bmp = dict.fromkeys(range(0x1000,sys.maxunicode+1),0xfffd)
 maxResult = 50
 playlistId = "PL_jWj0Wl8TG-UlSmo4HG3kDtTJYBO4UgB"
 video_list = []
 
 #copy paste url
-playlistURL = str(input('Input YouTube playlist URL here: '))
-#input is "https://www.youtube.com/playlist?list="
-playlistId = playlistURL[playlistURL.find('list=')+5:]
+playlistURL = str(input('Input full YouTube playlist URL here: '))
+#input is playlistID only, not full URL
 
 #forming query url
 playlist_url = "https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId="+playlistId+"&maxResults="+str(maxResult)+"&key="+API_KEY
-
+#print(playlist_url)
 #get playlist items ie. video id
 with urllib.request.urlopen(playlist_url) as url:
     data = json.loads(url.read().decode().translate(non_bmp))
@@ -64,9 +63,9 @@ print("Videos found: "+str(len(video_list)))
 counter = 0
 for i in video_list:
     query_url = "https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails&id=" + i + "&key=" + API_KEY
-
+    #print(query_url)
     with urllib.request.urlopen(query_url) as url:
-        data = json.loads(url.read().decode().translate(non_bmp))
+        data = json.loads(url.read().decode('utf-8'))
         #print(data['items'][0])
         try:
             allowed_r = len(data['items'][0]['contentDetails']['regionRestriction']['allowed'])
@@ -79,16 +78,26 @@ for i in video_list:
             #print("b: "+str(blocked_r))
         except:
             blocked_r = 0
-            
+
         #if regions == 0:
         #    print("[REGION FREE] "+data['items'][0]['snippet']['title'])
         #else:
         #    print(query_url)
         #    print("***REGION LOCKED***\n\t"+data['items'][0]['snippet']['title'])
-        if allowed_r > 0 or blocked_r > 0:
-            print("***REGION LOCKED***\n\t"+data['items'][0]['snippet']['title'])
-            #print(query_url)
-            print(i+"\n")
-            counter+=1
+        if allowed_r > 0:
+             print("ALLOWED IN "+ str(allowed_r) +" REGIONS: "+data['items'][0]['snippet']['title'])
+             #print(query_url)
+             print("*VideoID: "+i+"\n")
+             counter+=1
+        if blocked_r > 0:
+             print("BLOCKED IN "+ str(blocked_r) +" REGIONS: "+data['items'][0]['snippet']['title'])
+             #print(query_url)
+             print("*VideoID: "+i+"\n")
+             counter+=1
+        #if allowed_r > 0 or blocked_r > 0:
+        #    print("***REGION LOCKED IN "+ str(data['items'][0]['contentDetails']['regionRestriction']['blocked']) +"***\n\t"+data['items'][0]['snippet']['title'])
+        #    #print(query_url)
+        #    print(i+"\n")
+        #    counter+=1
 
 print(str(counter)+" may be region locked. Use the videoId provided for details.")
